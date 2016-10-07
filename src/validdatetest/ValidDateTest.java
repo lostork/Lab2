9a46f7ace3db54ac7a955c53ed67b12f4ce935d3
Ma Yifeng <993030067@qq.com>
2016-10-07 13:25:32 +0800

    validate date

---
+++ src/validdatetest/ValidDateTest.java
@@ -0,0 +1,30 @@
+package validdatetest;
+
+import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidDateTest {
+	public static boolean valiDateTimeWithLongFormat(String timeStr) {
+		String format = "((19|20)[0-9]{2})-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])";
+		Pattern pattern = Pattern.compile(format);
+		Matcher matcher = pattern.matcher(timeStr);
+		if (matcher.matches()) {
+			pattern = Pattern.compile("(\\d{4})-(\\d+)-(\\d+).*");
+			matcher = pattern.matcher(timeStr);
+			if (matcher.matches()) {
+				int y = Integer.valueOf(matcher.group(1));
+				int m = Integer.valueOf(matcher.group(2));
+				int d = Integer.valueOf(matcher.group(3));
+				if (d > 28) {
+					Calendar c = Calendar.getInstance();
+					c.set(y, m-1, 1);
+					int lastDay = c.getActualMaximum(Calendar.DAY_OF_MONTH);
+					return (lastDay >= d);
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+}
